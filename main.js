72c8413c8528c5b84290a32f6ffa2f60e3fdcc80
Waqas Ali <waqasansari4547@gmail.com>
2024-03-12 06:41:53 +0500

    Random Color Generator

---
+++ main.js
@@ -0,0 +1,13 @@
+const getColor = () => {
+  // Hexadecimal Code hota ha colors
+  const randomColor = Math.floor(Math.random() * 16777215);
+  const randomCode = '#' + randomColor.toString(16);
+//   console.log(randomColor,randomCode);
+  document.body.style.backgroundColor = randomCode;
+  document.getElementById('color-generator').innerText = randomCode;
+  navigator.clipboard.writeText(randomCode) // for copy text on Clipboard
+};
+document.getElementById("btn").addEventListener("click", getColor);
+
+// initial call
+getColor();
